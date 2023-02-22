24ccc4fbdd5fc7ee9841c1b20d80fbf61af83387
Larissa Amadeu <104395388+Larissa-Amadeu@users.noreply.github.com>
2023-02-22 15:54:57 -0300

    Update GerenciarUsuario.java

--- src/main/java/controle/GerenciarUsuario.java
+++ src/main/java/controle/GerenciarUsuario.java
@@ -3,13 +3,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package controle;
-
+import java.util.list;
+import modelo.Usuario;
 /**
  *
  * @author temporario
  */
 public class GerenciarUsuario {
     
+    private Usuario usuario;
+    private List<Usuario> lista;
     
     public void confirmarCadastro(){
         
