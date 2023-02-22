119e4dd8ee3e70f7793378c63df955114b4bf161
Larissa Amadeu <104395388+Larissa-Amadeu@users.noreply.github.com>
2023-02-22 15:57:14 -0300

    Update GerenciarReserva.java

--- src/main/java/controle/GerenciarReserva.java
+++ src/main/java/controle/GerenciarReserva.java
@@ -3,12 +3,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package controle;
-
+import java.util.List;
+import modelo.Reserva;
 
 public class GerenciarReserva {
     
+    private Reserva reserva;
+    private List<Reserva> lista;
+    
     public void realizarReserva(){
    }
+    
     public void cancelarReservaUsuario(){
         
 }
